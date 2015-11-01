d8e19ffd6321b991fd582639b4a4855c64cf9fde
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-01 21:14:35 +0200

    Add singleton design pattern solution in C#

---
+++ creational/singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Singleton
+{
+    private static readonly Singleton instance = new Singleton();
+
+    public static Singleton Instance
+    {
+        get 
+        {
+            return instance;
+        }
+    }
+
+    protected Singleton()
+    { }
+}
