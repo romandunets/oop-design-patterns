5592d1bc9cf8f874adf9ae7a489611487525e36b
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-02 18:40:05 +0200

    Add factory method design pattern solution in C#

---
+++ creational/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public abstract class AbstractFactory
+{
+    protected abstract IProduct CreateProduct();
+
+    public IProduct FactoryMethod()
+    {
+        return this.CreateProduct();
+    }
+}
+
+public class ConcreteFactory1 : AbstractFactory
+{
+    protected override IProduct CreateProduct()
+    {
+        return new ConcreteProduct1();
+    }
+}
+
+public class ConcreteFactory2 : AbstractFactory
+{
+    protected override IProduct CreateProduct()
+    {
+        return new ConcreteProduct2();
+    }
+}
+
+public interface IProduct
+{
+    void Launch();
+}
+
+public class ConcreteProduct1 : IProduct
+{
+    public void Launch()
+    {
+        Console.WriteLine("Launch Product 1");
+    }
+}
+
+public class ConcreteProduct2 : IProduct
+{
+    public void Launch()
+    {
+        Console.WriteLine("Launch Product 2");
+    }
+}
