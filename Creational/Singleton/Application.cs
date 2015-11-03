6996aefd4c3d2fb9e167d928604b1596430f9c21
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-03 19:57:00 +0200

    Add application class as an example how to use Singleton design pattern in C#

---
+++ Creational/Singleton/Application.cs
@@ -0,0 +1,20 @@
+﻿using Program;
+using System;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance;
+            Singleton instance2 = Singleton.Instance;
+
+            if (Object.ReferenceEquals(instance1, instance2))
+            {
+                Console.WriteLine("Both refereces links to one object");
+            }
+            Console.Read();
+        }
+    }
+}
