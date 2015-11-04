90e7715171975b219c06ab121b8ac7c68b7f8532
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-04 21:51:49 +0200

    Add main method as an example how to use abstract factory

---
+++ Creational/AbstractFactory/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AbstractFactory;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            IAbstractFactory factory1 = new ConcreteFactory1();
+            Client client1 = new Client(factory1);
+            client1.Launch();
+
+            IAbstractFactory factory2 = new ConcreteFactory2();
+            Client client2 = new Client(factory2);
+            client2.Launch();
+
+            Console.Read();
+        }
+    }
+}
