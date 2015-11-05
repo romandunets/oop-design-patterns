a62aca33e25da2a07f795116af24437fcfb527e9
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-05 22:00:46 +0200

    Add main method as an example how to use builder

---
+++ Creational/Builder/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Builder;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+
+            IBuilder builder1 = new ConcreteBuilder1();
+            IBuilder builder2 = new ConcreteBuilder2();
+
+            director.Construct(builder1);
+            Product product1 = builder1.GetResult();
+            product1.Show();
+
+            director.Construct(builder2);
+            Product product2 = builder2.GetResult();
+            product2.Show();
+
+            Console.Read();
+        }
+    }
+}
