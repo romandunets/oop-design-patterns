81b73ef44dbaa3afb38e77fc1eb24b8618eac9c9
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-08 13:06:54 +0200

    Add ChainOfResponsibility design pattern code in C#

---
+++ Behavioral/ChainOfResponsibility/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHandler handler = new ConcreteHandler1();
+            handler.Successor = new ConcreteHandler2();
+
+            handler.HandlerRequest("Question:What are You doing?");
+            handler.HandlerRequest("Answer:Nothing interesting.");
+        }
+    }
+
+    interface IHandler
+    {
+        IHandler Successor { get; set; }
+        void HandlerRequest(String request);
+    }
+
+    class ConcreteHandler1 : IHandler
+    {
+        public IHandler Successor { get; set; }
+
+        public void HandlerRequest(String request)
+        {
+            if (request.Contains("Question:"))
+            {
+                Console.WriteLine(request.Replace("Question:", ""));
+            }
+            else if (this.Successor != null)
+            {
+                this.Successor.HandlerRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandler2 : IHandler
+    {
+        public IHandler Successor { get; set; }
+
+        public void HandlerRequest(String request)
+        {
+            if (request.Contains("Answer:"))
+            {
+                Console.WriteLine(request.Replace("Answer:", ""));
+            }
+            else if (this.Successor != null)
+            {
+                this.Successor.HandlerRequest(request);
+            }
+        }
+    }
+}
