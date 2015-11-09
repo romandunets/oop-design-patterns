bc4aeed2e64748f3a0dfe3d230dfe5f542f170ec
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-09 23:36:18 +0200

    Fix example to use Prototype design pattern code in C#

--- Creational/Prototype/Application.cs
+++ Creational/Prototype/Application.cs
@@ -7,17 +7,19 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            IPrototype prototype1 = new ConcretePrototype1("One");
-            IPrototype clonePrototype1 = prototype1.Clone();
+            AbstractPrototype prototype1 = new ConcretePrototype1("One");
+            AbstractPrototype clonePrototype1 = prototype1.Clone();
 
-            IPrototype prototype2 = new ConcretePrototype1("Two");
-            IPrototype clonePrototype2 = prototype2.Clone();
+            AbstractPrototype prototype2 = new ConcretePrototype1("Two");
+            AbstractPrototype clonePrototype2 = prototype2.Clone();
 
             Console.WriteLine("First prototype: {0}", prototype1.GetId());
             Console.WriteLine("First prototype copy: {0}", clonePrototype1.GetId());
 
-            Console.WriteLine("First prototype: {0}", prototype2.GetId());
-            Console.WriteLine("First prototype copy: {0}", clonePrototype2.GetId());
+            Console.WriteLine("Second prototype: {0}", prototype2.GetId());
+            Console.WriteLine("Second prototype copy: {0}", clonePrototype2.GetId());
+
+            Console.Read();
         }
     }
 }
