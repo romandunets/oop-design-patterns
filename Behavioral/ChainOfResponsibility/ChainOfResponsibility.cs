c4d0593cb9ce108d43a0342d2f5cd10baa412967
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-10 21:12:09 +0200

    Separate pattern and application for ChainOfResponsibilities design pattern in C#

---
+++ Behavioral/ChainOfResponsibility/ChainOfResponsibility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    public interface IHandler
+    {
+        IHandler Successor { get; set; }
+        void HandlerRequest(string request);
+    }
+
+    public class ConcreteHandler1 : IHandler
+    {
+        public IHandler Successor { get; set; }
+
+        public void HandlerRequest(string request)
+        {
+            if (request.Contains("Question:"))
+            {
+                Console.WriteLine(request.Replace("Question:", ""));
+            }
+            else if (this.Successor != null)
+            {
+                this.Successor.HandlerRequest(request);
+            }
+        }
+    }
+
+    public class ConcreteHandler2 : IHandler
+    {
+        public IHandler Successor { get; set; }
+
+        public void HandlerRequest(string request)
+        {
+            if (request.Contains("Answer:"))
+            {
+                Console.WriteLine(request.Replace("Answer:", ""));
+            }
+            else if (this.Successor != null)
+            {
+                this.Successor.HandlerRequest(request);
+            }
+        }
+    }
+}
