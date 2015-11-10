103ae5abca520cdb0e4a0c88f883fc52b3d2867c
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-10 21:13:13 +0200

    Add application class as an example how to use ChainOfResponsibilities design pattern in C#

---
+++ Behavioral/ChainOfResponsibility/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ChainOfResponsibility;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            IHandler handler = new ConcreteHandler1();
+            handler.Successor = new ConcreteHandler2();
+
+            handler.HandlerRequest("Question:What are you doing?");
+            handler.HandlerRequest("Answer:Coding all the night.");
+
+            Console.Read();
+        }
+    }
+}
