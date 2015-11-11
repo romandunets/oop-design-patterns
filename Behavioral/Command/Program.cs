d8b1109953a73e604d028b9d8e3ba12151915c53
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-11 19:15:26 +0200

    Add Command design pattern code in C#

---
+++ Behavioral/Command/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Launch();
+        }
+    }
+
+    class Client
+    {
+        public void Launch()
+        {
+            Invoker invoker = new Invoker();
+            invoker.Action();
+        }
+    }
+
+    class Invoker
+    {
+        ICommand command;
+
+        public void Action()
+        {
+            command = new ConcreteCommand();
+            command.Execute();
+        }
+    }
+
+    interface ICommand
+    {
+        void Execute();
+    }
+
+    class ConcreteCommand : ICommand
+    {
+        private Receiver receiver = new Receiver();
+        private String state = "free";
+
+        public String State
+        {
+            get { return this.state; }
+        }
+
+        public void Execute()
+        {
+            state = "busy";
+            receiver.Action();
+            state = "free";
+        }
+    }
+
+    class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Some action...");
+        }
+    }
+}
