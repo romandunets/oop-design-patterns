ca4888e47e1e7008b8e5b37399ce05cbd9cd3b18
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-12 19:29:03 +0200

    Separate main application from the command pattern logic in C#

---
+++ Behavioral/Command/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Command;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Launch();
+        }
+    }
+}
--- Behavioral/Command/Command.cs
+++ Behavioral/Command/Command.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Command
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Client client = new Client();
-            client.Launch();
-        }
-    }
-
     class Client
     {
         public void Launch()
