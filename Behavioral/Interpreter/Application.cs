95cdafad51c5ec8c21a121280d40a8ef48b8e61e
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-13 20:37:09 +0200

    Add application class as an example how to use Interpreter design pattern in C#

---
+++ Behavioral/Interpreter/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Interpreter;
+using System.Collections.Generic;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            List<IExpression> expressions = new List<IExpression>();
+
+            expressions.Add(new TerminalExpression());
+            expressions.Add(new NonterminalExpression());
+            expressions.Add(new TerminalExpression());
+            expressions.Add(new TerminalExpression());
+
+            foreach (IExpression expression in expressions)
+            {
+                expression.Interpret(context);
+            }
+
+            Console.Read();
+        }
+    }
+}
