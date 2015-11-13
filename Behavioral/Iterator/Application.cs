f10aa11c1fc4e43de2a446cd2fa648eab9703fa0
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-13 21:07:42 +0200

    Fix Main method in Application class for Iterator design patterncode in C#

---
+++ Behavioral/Iterator/Application.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Iterator;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAggregate aggregate = new ConcreteAggregate();
+
+            aggregate[0] = "Item 1";
+            aggregate[1] = "Item 2";
+            aggregate[2] = "Item 3";
+            aggregate[3] = "Item 4";
+
+            ConcreteIterator iterator = new ConcreteIterator(aggregate);
+
+            object item = iterator.First();
+            while (!iterator.IsEnd())
+            {
+                Console.WriteLine(item);
+                item = iterator.Next();
+            }
+
+            Console.Read();
+        }
+    }
+}
