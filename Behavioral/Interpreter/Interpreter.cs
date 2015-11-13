32e161690b14d0a66c4206c7bc078d9ddd13e6e1
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-13 20:36:12 +0200

    Add Interpreter design pattern code in C#

---
+++ Behavioral/Interpreter/Interpreter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Interpreter
+{
+    public class Context
+    {
+    }
+
+    public interface IExpression
+    {
+        void Interpret(Context context);
+    }
+
+    public class TerminalExpression : IExpression
+    {
+        public void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal Expression...");
+        }
+    }
+
+    public class NonterminalExpression : IExpression
+    {
+        public void Interpret(Context context)
+        {
+            Console.WriteLine("Called Nonterminal Expression...");
+        }
+    }
+}
