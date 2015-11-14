b6419651b63f3dc8c365f6ddded1a4ea667da007
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-14 18:45:05 +0200

    Add application class as an example how to use Mediator design pattern in C#

---
+++ Behavioral/Mediator/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mediator;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator mediator = new ConcreteMediator();
+
+            ConcreteColleague1 colleague1 = new ConcreteColleague1(mediator);
+            ConcreteColleague2 colleague2 = new ConcreteColleague2(mediator);
+
+            mediator.Colleague1 = colleague1;
+            mediator.Colleague2 = colleague2;
+
+            colleague1.Send("Message 1");
+            colleague2.Send("Message 2");
+
+            Console.Read();
+        }
+    }
+}
