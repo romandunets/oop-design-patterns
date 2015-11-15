3a292d77e808a1ac03227d4dde38246b0f3c1854
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-15 19:03:14 +0200

    Add application class as an example how to use State design pattern in C#

---
+++ Behavioral/State/Application.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using State;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStateA());
+
+            Console.WriteLine(context.State);
+            context.Request();
+            Console.WriteLine(context.State);
+            context.Request();
+            Console.WriteLine(context.State);
+            context.Request();
+            Console.WriteLine(context.State);
+
+            Console.Read();
+        }
+    }
+}
