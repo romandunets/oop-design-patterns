65ada24335f67f69a5b328594de2a1d0791c17a3
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-15 18:42:34 +0200

    Add application class as an example how to use Observer design pattern in C#

---
+++ Behavioral/Observer/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Observer;
+
+namespace Observer
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject concreteSubject = new ConcreteSubject();
+
+            concreteSubject.Attach(new ConcreteObserver(concreteSubject, "Observer A"));
+            concreteSubject.Attach(new ConcreteObserver(concreteSubject, "Observer B"));
+            concreteSubject.Attach(new ConcreteObserver(concreteSubject, "Observer C"));
+
+            concreteSubject.State = "Loading...";
+            concreteSubject.Notify();
+
+            Console.Read();
+        }
+    }
+}
