b93fcbcc9af7c3b62003d0376b5022cf7e7b7866
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-15 19:03:00 +0200

    Add State design pattern code in C#

---
+++ Behavioral/State/State.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace State
+{
+    public interface IState
+    {
+        void Handle(Context context);
+    }
+
+    public class ConcreteStateA : IState
+    {
+        public void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    public class ConcreteStateB : IState
+    {
+        public void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    public class Context
+    {
+        public IState State { get; set; }
+
+        public Context(IState state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+
+    interface IObject
+    {
+        void Stage1();
+        void Stage2();
+        void Stage3();
+    }
+}
