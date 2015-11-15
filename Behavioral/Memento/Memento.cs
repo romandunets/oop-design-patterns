ef868505e4449911069865b26ee5a6e72b752312
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-15 16:53:16 +0200

    Add Memento design pattern code in C#

---
+++ Behavioral/Memento/Memento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Originator
+    {
+        public string State { get; set; }
+
+        public Memento CreateMemento()
+        {
+            return (new Memento(State));
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+
+    public class Memento
+    {
+        public string State { get; protected set; }
+
+        public Memento(string state)
+        {
+            this.State = state;
+        }
+    }
+
+    public class Caretaker
+    {
+        protected Memento memento;
+
+        public Memento Memento
+        {
+            set { memento = value; }
+            get { return memento; }
+        }
+    }
+}
