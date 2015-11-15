e04c650f9b240dfcdb1e9b5fe7738702c13c47ab
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-15 16:53:33 +0200

    Add application class as an example how to use Memento design pattern in C#

---
+++ Behavioral/Memento/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Memento;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Originator originator = new Originator();
+            originator.State = "On";
+
+            Console.WriteLine(originator.State);
+
+            Caretaker caretaker = new Caretaker();
+            caretaker.Memento = originator.CreateMemento();
+
+            originator.State = "Off";
+
+            Console.WriteLine(originator.State);
+
+            originator.SetMemento(caretaker.Memento);
+
+            Console.WriteLine(originator.State);
+            Console.Read();
+        }
+    }
+}
