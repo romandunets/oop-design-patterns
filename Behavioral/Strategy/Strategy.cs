40b011bb5592700650373b9c36939193f5aba9e9
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-16 20:46:51 +0200

    Add Strategy design pattern code in C#

---
+++ Behavioral/Strategy/Strategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Strategy
+{
+    public class Context
+    {
+        public IStrategy Strategy { get; set; }
+
+        public Context(IStrategy strategy)
+        {
+            Strategy = strategy;
+        }
+
+        public void Execute()
+        {
+            Strategy.BehaviorInterface();
+        }
+    }
+
+    public interface IStrategy
+    {
+        void BehaviorInterface();
+    }
+
+    public class ConcreteStrategyA : IStrategy
+    {
+        public void BehaviorInterface()
+        {
+            Console.WriteLine("Algorithm 1 is working...");
+        }
+    }
+
+    public class ConcreteStrategyB : IStrategy
+    {
+        public void BehaviorInterface()
+        {
+            Console.WriteLine("Algorithm 2 is working...");
+        }
+    }
+}
