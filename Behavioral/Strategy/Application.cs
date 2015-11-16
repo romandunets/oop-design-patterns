d02983199cce210537db2df9b035eae6b2459110
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-16 20:47:08 +0200

    Add application class as an example how to use Strategy design pattern in C#

---
+++ Behavioral/Strategy/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Strategy;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStrategyA());
+            context.Execute();
+
+            context.Strategy = new ConcreteStrategyB();
+            context.Execute();
+
+            Console.Read();
+        }
+    }
+}
