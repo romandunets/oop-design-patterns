88267af205ef1d29b157d39a4f533a97322ddd4c
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-17 18:53:43 +0200

    Add application class as an example how to use Visitor design pattern in C#

---
+++ Behavioral/Visitor/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Visitor;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            ObjectStructure objectStructure = new ObjectStructure();
+
+            objectStructure.Attach(new ConcreteElementA());
+            objectStructure.Attach(new ConcreteElementB());
+
+            ConcreteVisitor1 concreteVisitor1 = new ConcreteVisitor1();
+            ConcreteVisitor2 concreteVisitor2 = new ConcreteVisitor2();
+
+            objectStructure.Accept(concreteVisitor1);
+            objectStructure.Accept(concreteVisitor2);
+
+            Console.Read();
+        }
+    }
+}
