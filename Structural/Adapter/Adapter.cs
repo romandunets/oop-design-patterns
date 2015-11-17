c78e1d075c2253e138925cfabf093f9aed7dea51
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-17 19:18:44 +0200

    Add Adapter design pattern code in C#

---
+++ Structural/Adapter/Adapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPattern
+{
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Target request");
+        }
+    }
+
+    public class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Specific request");
+        }
+    }
+}
