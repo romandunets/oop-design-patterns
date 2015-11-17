b2d38067d12461b581f017066aa1a18efcb1f74a
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-17 18:25:49 +0200

    Add application class as an example how to use TemplateMethod design pattern in C#

---
+++ Behavioral/TemplateMethod/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TemplateMethod;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass abstractClass = new ConcreteClassA();
+            abstractClass.Launch();
+
+            abstractClass = new ConcreteClassB();
+            abstractClass.Launch();
+
+            Console.Read();
+        }
+    }
+}
