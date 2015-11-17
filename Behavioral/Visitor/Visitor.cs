cb94a98d42fc7ac7a13e3314b122b38b3e635421
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-17 18:53:05 +0200

    Add Visitor design pattern code in C#

---
+++ Behavioral/Visitor/Visitor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Visitor
+{
+    public interface IVisitor
+    {
+        void Visit(ConcreteElementA element);
+        void Visit(ConcreteElementB element);
+    }
+
+    public class ConcreteVisitor1 : IVisitor
+    {
+        public void Visit(ConcreteElementA element)
+        {
+            Console.WriteLine("Visitor 1 is visiting {0}", element.GetType().Name);
+        }
+
+        public void Visit(ConcreteElementB element)
+        {
+            Console.WriteLine("Visitor 1 is visiting {0}", element.GetType().Name);
+        }
+    }
+
+    public class ConcreteVisitor2 : IVisitor
+    {
+        public void Visit(ConcreteElementA element)
+        {
+            Console.WriteLine("Visitor 2 is visiting {0}", element.GetType().Name);
+        }
+
+        public void Visit(ConcreteElementB element)
+        {
+            Console.WriteLine("Visitor 2 is visiting {0}", element.GetType().Name);
+        }
+    }
+
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class ConcreteElementA : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class ConcreteElementB : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class ObjectStructure
+    {
+        protected List<IElement> elements = new List<IElement>();
+
+        public void Attach(IElement element)
+        {
+            elements.Add(element);
+        }
+
+        public void Detach(IElement element)
+        {
+            elements.Remove(element);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (IElement element in elements)
+            {
+                element.Accept(visitor);
+            }
+        }
+    }
+}
