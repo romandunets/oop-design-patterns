4dd45ed5d72dc068c2021b0b8cd975e81325ceed
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-18 21:09:15 +0200

    Add application class as an example how to use Composite design pattern in C#

---
+++ Structural/Composite/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CompositePattern;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("root");
+
+            root.AddChild(new Leaf("Leaf 1-A"));
+
+            Composite composite = new Composite("Composite 1-B");
+            composite.AddChild(new Leaf("Leaf 2-A"));
+            composite.AddChild(new Leaf("Leaf 2-B"));
+
+            root.AddChild(composite);
+            root.AddChild(new Leaf("Leaf 1-C"));
+
+            root.doOperation();
+            Console.Read();
+        }
+    }
+}
