f38c1321722ae98de0ac024f8f957844656b6205
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-18 21:38:18 +0200

    Add application class as an example how to use Decorator design pattern in C#

---
+++ Structural/Decorator/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DecoratorPattern;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            ConcreteComponent concreteComponent = new ConcreteComponent();
+            ConcreteDecoratorA concreteDecoratorA = new ConcreteDecoratorA();
+            ConcreteDecoratorB concreteDecoratorB = new ConcreteDecoratorB();
+
+            concreteDecoratorA.Component = concreteComponent;
+            concreteDecoratorB.Component = concreteDecoratorA;
+
+            concreteDecoratorB.Operation();
+
+            Console.Read();
+        }
+    }
+}
