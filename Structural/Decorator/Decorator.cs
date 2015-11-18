bd726e679fff9deacdcac540a16e04c48696c266
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-18 21:32:39 +0200

    Add Decorator design pattern code in C#

---
+++ Structural/Decorator/Decorator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    public interface IComponent
+    {
+        void Operation();
+    }
+
+    public class ConcreteComponent : IComponent
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Concrete operation...");
+        }
+    }
+
+    public abstract class Decorator : IComponent
+    {
+        public IComponent Component { get; set; }
+
+        public virtual void Operation()
+        {
+            if (Component != null)
+            {
+                Component.Operation();
+            }
+        }
+    }
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Extended operation 1...");
+        }
+    }
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Extended operation 2...");
+        }
+    }
+}
