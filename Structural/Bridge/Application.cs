9b5f2841c1da01ef4e90e08e7e94bf7643af1158
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-18 20:35:53 +0200

    Add application class as an example how to use Bridge design pattern in C#

---
+++ Structural/Bridge/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Bridge;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Abstraction abstraction = new RefinedAbstraction();
+
+            abstraction.Implementor = new ConcreteImplementorA();
+            abstraction.Operation();
+
+            abstraction.Implementor = new ConcreteImplementorB();
+            abstraction.Operation();
+
+            Console.Read();
+        }
+    }
+}
