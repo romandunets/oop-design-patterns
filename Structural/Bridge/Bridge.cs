96d42357c38cd904489ba84fe786ef45227e77ce
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-18 20:35:00 +0200

    Add Bridge design pattern code in C#

---
+++ Structural/Bridge/Bridge.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Bridge
+{
+    public class Abstraction
+    {
+        protected IImplementor implementor;
+
+        public IImplementor Implementor
+        {
+            get { return implementor; }
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    public class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    public interface IImplementor
+    {
+        void Operation();
+    }
+
+    public class ConcreteImplementorA : IImplementor
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Concrete operation A");
+        }
+    }
+
+    public class ConcreteImplementorB : IImplementor
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Concrete operation B");
+        }
+    }
+}
