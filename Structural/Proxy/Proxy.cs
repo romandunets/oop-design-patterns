b6db9370ea11988aba8076a8ba4d0acac807dd40
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-19 22:23:46 +0200

    Add Proxy design pattern code in C#

---
+++ Structural/Proxy/Proxy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class Clinet
+    {
+        public void Launch()
+        {
+            ISubject subject = new Proxy();
+            subject.DoOperation();
+        }
+    }
+
+    public interface ISubject
+    {
+        void DoOperation();
+    }
+
+    public class RealSubject : ISubject
+    {
+        public void DoOperation()
+        {
+            Console.WriteLine("RealSubject operation...");
+        }
+    }
+
+    public class Proxy : ISubject
+    {
+        protected RealSubject realSubject;
+
+        public void DoOperation()
+        {
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+
+            realSubject.DoOperation();
+        }
+    }
+}
