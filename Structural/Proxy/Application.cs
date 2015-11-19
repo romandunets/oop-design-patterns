3e175c4c7c1702db831f11e8fba37b30909842b3
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-19 22:24:21 +0200

    Add application class as an example how to use Proxy design pattern in C#

---
+++ Structural/Proxy/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Proxy;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Clinet client = new Clinet();
+            client.Launch();
+
+            Console.Read();
+        }
+    }
+}
