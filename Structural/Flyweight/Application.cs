7b2b0d76eb6912e92ed6fc0f977eae9392020318
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-19 20:11:36 +0200

    Add application class as an example how to use Flyweight design pattern in C#

---
+++ Structural/Flyweight/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FlyweightPattern;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Launch();
+
+            Console.Read();
+        }
+    }
+}
