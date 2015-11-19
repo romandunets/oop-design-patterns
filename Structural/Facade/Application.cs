02df11e9869df7f39cbff02b2d947404ca9d2689
Roman Dunets <roman.dunets.email@gmail.com>
2015-11-19 19:43:35 +0200

    Add application class as an example how to use Facade design pattern in C#

---
+++ Structural/Facade/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FacadePattern;
+
+namespace Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+
+            facade.Operation1();
+            facade.Operation2();
+            facade.Operation3();
+
+            Console.Read();
+        }
+    }
+}
